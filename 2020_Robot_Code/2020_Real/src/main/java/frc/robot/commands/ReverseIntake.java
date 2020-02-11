4d640c8e73c5916076e7d6f76aef608352f25048
Vikram <alexgiantseed@gmail.com>
2020-02-10 18:48:09 -0800

    return true stuff

--- 2020_Robot_Code/2020_Real/src/main/java/frc/robot/commands/ReverseIntake.java
+++ 2020_Robot_Code/2020_Real/src/main/java/frc/robot/commands/ReverseIntake.java
@@ -50,6 +50,6 @@ public class ReverseIntake extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
