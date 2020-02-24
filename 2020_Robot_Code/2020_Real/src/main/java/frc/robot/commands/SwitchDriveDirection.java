3cc7e7fa4faa935f36b599dbcd1767a892462985
Adarsh <adroyalz@gmail.com>
2020-02-24 10:43:04 -0800

    Update SwitchDriveDirection.java

--- 2020_Robot_Code/2020_Real/src/main/java/frc/robot/commands/SwitchDriveDirection.java
+++ 2020_Robot_Code/2020_Real/src/main/java/frc/robot/commands/SwitchDriveDirection.java
@@ -26,14 +26,15 @@ public class SwitchDriveDirection extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  //@Override
+  public void execute() {
     m_driveSubsystem.switchDriveDirection();
     //System.out.println("command inititalized");
   }
-
-  // // Called every time the scheduler runs while the command is scheduled.
-  // //@Override
-  // public void execute() {
-  // }
 
   // // Called once the command ends or is interrupted.
   // //@Override
